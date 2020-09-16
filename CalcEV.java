4bd88faa5ec4a96302709b1df799f245a7d47f50
wwtse <tsengmign@outlook.com>
2020-09-16 01:50:04 -0400

    TO DO 1

---
+++ CalcEV.java
@@ -0,0 +1,6 @@
+public class CalcEV {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ Process.java
@@ -0,0 +1,32 @@
+public class Process {
+    private int[] cards;
+    private int[] dcards;
+    private int[] cards1;
+    private int[] cards2;
+    private int[] cards3;
+    private int[] cards4;
+
+    /** TODO init the cards*/
+    private Process(){
+        cards = new int[58];
+
+    }
+
+    /** TODO delete one card randomly and create dcards
+     * 2nd mission is must or ... ?*/
+    private void deleteRandom(){
+
+    }
+
+    /** TODO  deal */
+    private void  dealer(){}
+
+    /** TODO  delete the same number card
+     * does it need create the */
+    private void throwSame(){}
+
+    /** TODO  return size */
+    public double calcCardNum(){
+        return 0.0;
+    }
+}
